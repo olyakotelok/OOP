5fd2229990dcabff7f9be530041dabf391bec338
Nikolaev Evgeny <eannikolaev@gmail.com>
2019-11-27 08:59:59 +0500

    working input

--- src/bot/console/Welcome.java
+++ src/bot/console/Welcome.java
@@ -1,11 +1,13 @@
-package bot.console;
+import java.util.Scanner;
 
 public class Welcome {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! There will be information about me. \n\n What topic is interesting for you?");
-        System.out.println("1. History \n 2.Physics");
-        System.out.print("Enter number:");
-        int topic = System.in.read();
+        System.out.println("1. History \n2. Physics");
+        System.out.print("Enter number: ");
+        int topic = scanner.nextInt();
+        System.out.println(topic);
         if (topic == 1) {
             System.out.println("Okay, your choice is History!");
         }
