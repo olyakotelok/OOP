99554bbfc1f51ad634ffbbcfa26c0c319330c826
olyakotelok <43614432+olyakotelok@users.noreply.github.com>
2019-12-25 12:54:02 +0500

    Add files via upload

---
+++ bot2/src/main/java/com/company/interfaces/ICommunicationType.java
@@ -0,0 +1,7 @@
+package com.company.interfaces;
+
+
+public interface ICommunicationType
+{
+    void getMsg(String update);
+}
---
+++ bot2/src/main/java/com/company/interfaces/IGame.java
@@ -0,0 +1,12 @@
+package com.company.interfaces;
+
+public interface IGame
+{
+    String start();
+    void readMessage(String str);
+    String getMessage();
+    String getName();
+    boolean isFinished();
+    String getLastMessage();
+    String getHelp();
+}
